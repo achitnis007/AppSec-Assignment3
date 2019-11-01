aefeb01d5d08de4cccad8727a0440272506d67ee
achitnis007 <abhijit.chitnis@gmail.com>
2019-11-01 18:26:01 -0400

    ongoing changes to Assgn-3

--- app/__init__.py
+++ app/__init__.py
@@ -36,7 +36,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
-# db.init_app(app)
 
 from app import db
 
