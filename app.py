a13bcb7db182a93a30e2c533a8b6e3f07a5cf391
achitnis007 <abhijit.chitnis@gmail.com>
2019-11-06 17:38:50 -0500

    Assgn-3 - changes for login activity history

--- app.py
+++ app.py
@@ -9,17 +9,17 @@ hashed_password = bcrypt.generate_password_hash('Administrator@1').decode('utf-8
 user = User(username='admin', phone='12345678901', password=hashed_password)
 db.session.add(user)
 
-# hashed_password = bcrypt.generate_password_hash('1111111111').decode('utf-8')
-# user = User(username='tester1', phone='1111111111', password=hashed_password)
-# db.session.add(user)
-#
-# hashed_password = bcrypt.generate_password_hash('2222222222').decode('utf-8')
-# user = User(username='tester2', phone='2222222222', password=hashed_password)
-# db.session.add(user)
-#
-# hashed_password = bcrypt.generate_password_hash('3333333333').decode('utf-8')
-# user = User(username='tester3', phone='3333333333', password=hashed_password)
-# db.session.add(user)
+hashed_password = bcrypt.generate_password_hash('1111111111').decode('utf-8')
+user = User(username='actester1', phone='1111111111', password=hashed_password)
+db.session.add(user)
+
+hashed_password = bcrypt.generate_password_hash('2222222222').decode('utf-8')
+user = User(username='actester2', phone='2222222222', password=hashed_password)
+db.session.add(user)
+
+hashed_password = bcrypt.generate_password_hash('3333333333').decode('utf-8')
+user = User(username='actester3', phone='3333333333', password=hashed_password)
+db.session.add(user)
 
 db.session.commit()
 
